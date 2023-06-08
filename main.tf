4262af6d03303d660641ed20b1404eed2f620e57
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-06-08 16:15:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
    #client_id = "1244ff47-5233-442c-b8df-b761e220bc23"
    #client_secret = "YtY8Q~Q6kLoKDGGGEn3lGlILr--HZ5EsbJwOGba-"
    #tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
-   skip_provider_registration = true
+   #skip_provider_registration = true
    features {}
 }
 
